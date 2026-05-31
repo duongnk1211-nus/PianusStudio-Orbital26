9a01ccd4401eea50152b3f28f2c2878e3858ffbc
duongnk1211-nus <duongnguyenkhanh2007@gmail.com>
2026-05-31 09:07:28 +0700

    added background image to piano page and made the return button.

--- pianus-studio/src/components/PianoKeyBoard.jsx
+++ pianus-studio/src/components/PianoKeyBoard.jsx
@@ -6,13 +6,14 @@ import { Notes, getKeyMap, computedLeftForNote } from "./kbComps/noteList.jsx";
 import { setTone } from "./kbComps/setTone.jsx";
 import { handleKeyDown, handleKeyUp, handleMouseDown, handleMouseUp } from "./kbComps/movementHandler.jsx";
 import { attack, release } from "./kbComps/soundDisplayer.jsx";
+import { Link } from "react-router-dom";
 
 const NOTES = Notes;
 
 const KeyMap = getKeyMap();
 
 //Handling key press
-export default function PianoKeyBoard(){
+export default function PianoKeyBoard() {
   const [activeKeys, setActiveKeys] = useState(new Set());
 
   const synthRef = useRef(null);
@@ -24,7 +25,7 @@ export default function PianoKeyBoard(){
     setTone(synthRef);
 
     const GROW_SPEED = 1;    // px per frame while held
-    const FLY_SPEED  = 1;    // px per frame after release
+    const FLY_SPEED = 1;    // px per frame after release
 
     const tick = () => {
       barsRef.current = barsRef.current
@@ -55,50 +56,57 @@ export default function PianoKeyBoard(){
   }, []);
 
   return (
-    <div className="piano-wrapper">
-      <div className="synthesia-container">
-        {displayBars.map(b => (
-          <div
-            key={b.id}
-            className={`synthesia-bar ${b.type}`}
-            style={{
-              left: `${b.left}px`,
-              width: `${b.width - 2}px`,
-              height: `${b.height}px`,
-              top: `${b.top}px`,
-            }}
-          />
-        ))}
+    <div className="piano-container">
+      <div className="return-button">
+        <Link to="/">Return</Link>
       </div>
-      <div className="key-rows">
-        {NOTES.filter(n => n.type === "white").map((n) => (
-          <div
-            key={n.note}
-            onMouseDown={ handleMouseDown(synthRef, barsRef, setActiveKeys, n) }
-            onMouseUp={ handleMouseUp(synthRef, barsRef, setActiveKeys, n) }
-            className={`white-key ${activeKeys.has(n.key) ? 'active' : ''}`}
-          >
-            <span className="white-key-letter-label">
-              {n.key.toUpperCase()}
-            </span>
-          </div>
-        ))}
-        {NOTES.filter(n => n.type === "black").map((n) => {
-          const left = computedLeftForNote(n);
-          return (
+      <div className="piano-wrapper">
+        <div className="synthesia-container">
+          {displayBars.map(b => (
+            <div
+              key={b.id}
+              className={`synthesia-bar ${b.type}`}
+              style={{
+                left: `${b.left}px`,
+                width: `${b.width - 2}px`,
+                height: `${b.height}px`,
+                top: `${b.top}px`,
+              }}
+            />
+          ))}
+        </div>
+        <div className="key-rows">
+          {NOTES.filter(n => n.type === "white").map((n) => (
             <div
               key={n.note}
-              className={`black-key ${activeKeys.has(n.key) ? 'active' : ''}`}
-              style={{ left: `${left}px` }}
-              onMouseDown={ handleMouseDown(synthRef, barsRef, setActiveKeys, n) }
-              onMouseUp={ handleMouseUp(synthRef, barsRef, setActiveKeys, n) }
+              onMouseDown={handleMouseDown(synthRef, barsRef, setActiveKeys, n)}
+              onMouseUp={handleMouseUp(synthRef, barsRef, setActiveKeys, n)}
+              className={`white-key ${activeKeys.has(n.key) ? 'active' : ''}`}
             >
-              <span className="black-key-letter-label">
+              <span className="white-key-letter-label">
                 {n.key.toUpperCase()}
               </span>
             </div>
-          );
-        })}
+          ))}
+
+
+          {NOTES.filter(n => n.type === "black").map((n) => {
+            const left = computedLeftForNote(n);
+            return (
+              <div
+                key={n.note}
+                className={`black-key ${activeKeys.has(n.key) ? 'active' : ''}`}
+                style={{ left: `${left}px` }}
+                onMouseDown={handleMouseDown(synthRef, barsRef, setActiveKeys, n)}
+                onMouseUp={handleMouseUp(synthRef, barsRef, setActiveKeys, n)}
+              >
+                <span className="black-key-letter-label">
+                  {n.key.toUpperCase()}
+                </span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
